dcb9d059a3bf0462246dc3ce3f70d76343248f42
chenfy1985@gmail.com <chenfy1985@gmail.com@d9519993-b70f-e74c-374e-1cc9b44e889f>
2012-11-07 05:47:02 +0000

    加入短信服务的模块

---
+++ trunk/Vs08SMSService/Vs08SMSService/Log.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Vs08SMSService
+{
+    /// <summary>
+    /// 实现写日志的类
+    /// 时间：2008-3-17
+    /// </summary>
+    public class Log
+    {
+        private string log_file_path = null;
+        /// <summary>
+        /// 获取或设置日志文件的路径
+        /// </summary>
+        public string FilePath
+        {
+            get
+            {
+                return log_file_path;
+            }
+            set
+            {
+                log_file_path = value;
+            }
+        }
+        public Log(string filePath)
+        {
+            log_file_path = filePath;
+        }
+        public Log()
+        {
+            log_file_path = null;
+        }
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public void WriteLog(string log)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                if (log_file_path != null)
+                {
+                    sw = new StreamWriter(log_file_path, true, System.Text.Encoding.Default);
+                    sw.WriteLine(System.DateTime.Now.ToShortTimeString() + " : " + log);
+                    sw.Close();
+                    sw = null;
+                }
+            }
+            catch (Exception) { }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+        }
+    }
+
+    public class ShortUnit
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ trunk/Vs08SMSService/Vs08SMSService/Service1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Vs08SMSService
+{
+    partial class Service1
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.Source = "fileSystemWatcher1";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "*.txt";
+            this.fileSystemWatcher1.Path = "C:\\";
+            // 
+            // Service1
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
